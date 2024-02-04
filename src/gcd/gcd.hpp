81683681a352690e29cd2362a82f4192f9fd321b
ldcas-uc3m <78721925+ldcas-uc3m@users.noreply.github.com>
2024-02-04 18:20:04 +0100

    fix c++ library + better support for linux

--- src/gcd/gcd.hpp
+++ src/gcd/gcd.hpp
@@ -2,8 +2,14 @@
 #define GCD_HPP
 
 
+#include <iostream>
+#include <cmath>
 #include <concepts>
 #include <map>
+#include <stdexcept>
+#include <string>
+#include <random>
+#include <chrono>
 
 
 
@@ -11,29 +17,101 @@ template <std::integral INT_T>
 /**
 * @brief Returns the prime factorials of a natural number n, and their exponents (frequency)
 */
-std::map<INT_T, INT_T> factorize(INT_T n);
+std::map<INT_T, INT_T> factorize(INT_T n) {
+    if (n <= 0) throw std::invalid_argument("n must be bigger than 0");
+
+    INT_T i = 2;
+    std::map<INT_T, INT_T> factors {};  // prime factors and their exponents
+
+    while (n > 1) {
+        if (n % i == 0) {  // found a prime factor
+
+            // update exponent
+            if (factors.contains(i))
+                ++factors[i];
+            else
+                factors.insert({i, 1});
+
+            n /= i;
+        }
+        else ++i;
+    }
+
+    return factors;
+}
 
 
 template <std::integral INT_T>
 /**
 * @brief Returns the Greatest Common Denominator in between a and b
 */
-INT_T gcd_factorize(INT_T a, INT_T b);
+INT_T gcd_factorize(INT_T a, INT_T b) {
 
+    // base cases
+
+    if (b == 0)
+        return a;
+
+    if (a == 0)
+        return b;
+
+    if (a == b)
+        return a;
+
+    if ((a <= 1) || (b <= 1))
+        return 1;
+
+
+    // figure out biggest and smallest number
+    INT_T n = std::max(a, b);
+    INT_T m = std::min(a, b);
+
+
+    // get factors for both numbers
+    std::map<INT_T, INT_T> factors_n = factorize(n);
+    std::map<INT_T, INT_T> factors_m = factorize(m);
+
+
+    // go through the factors of m, if it's a common factor, multiply the result by the factor to the smallest exponent
+
+    INT_T res = 1;
+
+    for (const auto & [factor, exponent] : factors_m) {
+        if (factors_n.contains(factor))
+            res *= std::pow(factor, std::min(exponent, factors_n[factor]));
+    }
+
+    return res;
+}
 
 
 template <std::integral INT_T>
 /**
 * @brief Returns the Greatest Common Denominator in between a and b
 */
-INT_T gcd_euclid(INT_T a, INT_T b);
+INT_T gcd_euclid(INT_T a, INT_T b) {
+    if (a == b) return a;
+
+    while (b > 0) {
+        INT_T r = a % b;
+
+        // gcd_euclid(m, r)
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
 
 
 template <std::integral INT_T>
 /**
 * @brief Returns the Greatest Common Denominator in between a and b
 */
-INT_T gcd_euclid_rec(INT_T a, INT_T b);
+INT_T gcd_euclid_rec(INT_T a, INT_T b) {
+    if (b == 0) return a;
 
+    return gcd_euclid_rec(b, a % b);
+}
 
 #endif
