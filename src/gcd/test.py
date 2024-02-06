3f82f59b8f8fe3afbaf81387e536f53918b7bc94
Iarnaiz <78861772+Iarnaiz@users.noreply.github.com>
2024-02-06 21:10:52 +0100

    CSV file saving added

--- src/gcd/test.py
+++ src/gcd/test.py
@@ -5,6 +5,8 @@ import gcdlib
 from time import perf_counter
 import random
 import matplotlib.pyplot as plt
+from statistics import mean
+import csv
 
 #print(gcdlib.gcd_factorize(24, 36))
 
@@ -20,12 +22,20 @@ def calc_performance():
 
 
     while maxN < 10000000000:
-        inicio_tiempo = perf_counter()
-        resultado_actual = gcdlib.gcd_factorize(a, b)
-        fin_tiempo = perf_counter()
+
+        counter = 0
+        avgTime = []
+
+        #For it length of number calculates it 10 times and get the mean value for calculation time
+        while counter <= 9:
+            inicio_tiempo = perf_counter()
+            resultado_actual = gcdlib.gcd_factorize(a, b)
+            fin_tiempo = perf_counter()
+            avgTime.append(fin_tiempo - inicio_tiempo)
+            counter+=1
 
         numeros.append(len(str(maxN)))
-        tiempos.append(fin_tiempo - inicio_tiempo)
+        tiempos.append(mean(avgTime))
 
         print(f"Iteración: MCD({a}, {b}) = {resultado_actual}")
         print(f"Tiempo de ejecución: {fin_tiempo - inicio_tiempo} segundos\n")
@@ -39,12 +49,12 @@ def calc_performance():
     return numeros, tiempos
 
 
-
+#The data obtained for time consumption to calculate number of different number of digits
 def plot_performance(numeros, tiempos):
     
     plt.figure(figsize=(6, 4))
 
-    # Crear un gráfico de dispersión
+    # Crear gráfico
     plt.plot(numeros, tiempos, marker='o', color='blue', linestyle='-')
     plt.title('Relación entre Longitud del Número y Tiempo de Ejecución')
     plt.xlabel('Longitud del Número')
@@ -55,12 +65,24 @@ def plot_performance(numeros, tiempos):
 
 
 
+def saveCSV(numbers, times):
+
+    data = zip(numbers, times)
+
+    # Write to CSV file
+    with open('output.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Digits', 'Time'])  
+        csv_writer.writerows(data)  
+
+
 
 perf_results, perf_times = calc_performance()
 
-
-# Crear gráfico de dispersión
+# Función para generar el grafico a aprtir de los datos calculados
 plot_performance(perf_results, perf_times)
+
+saveCSV(perf_results, perf_times)
 
 
 print("Resultados:", perf_results)
