db2b34494b9ae59f3dfaf5ad7a63cd25a4a9e09c
ldcas-uc3m <100429021@alumnos.uc3m.es>
2024-02-10 17:57:21 +0100

    HITO 1
    final touches

--- src/primes/test.py
+++
@@ -1,6 +0,0 @@
-
-import primeslib
-
-
-print(primeslib.MAX_UINT)
-print(primeslib.is_prime(17131175322357111317))
--- zip.sh
+++ zip.sh
@@ -2,7 +2,7 @@
 # Bash script to zip the whole project in order to make it deriverable
 # please make sure zip, pv and texlive are installed
 
-OUTFILE=../outfile.zip
+OUTFILE="../EP1 - 3-Arnaiz-Casais.zip"
 
 
 # compile the report (and save it to root folder)
@@ -13,15 +13,9 @@ latexmk -cd -shell-escape -pdf report/report.tex
 cp report/report.pdf .
 
 
-cd src
-
-# <clean stuff up>
-
-cd ..
-
 # zip it (excluding useless stuff)
 echo "Zipping..."
-zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* venv/\* LICENSE README.md
+zip -r "$OUTFILE" . -x zip.sh report/\* \*.git\* img/\* venv/\* build/\* .vscode/\* LICENSE README.md
 
 # cleanup
 echo "Cleaning up..."
