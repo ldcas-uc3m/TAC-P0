11c0b396b31c1795b55c5ae9b88c6e05775b680f
Iarnaiz <78861772+Iarnaiz@users.noreply.github.com>
2024-02-16 22:07:08 +0100

    primes graph fixed

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ import random
 from typing import Callable
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 import gcdlib
@@ -119,8 +120,7 @@ def plot_performance(numeros, tiempos, filename: str, title: str, xlabel: str, y
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
-    plt.xticks(range(int(min(numeros)), int(max(numeros))+1))
-    plt.yticks(range(int(min(tiempos)), int(max(tiempos))+1))
+    plt.xticks(range(len(numeros)), [str(n) for n in numeros])
 
     # Mostrar el gráfico
     plt.savefig(f'{IMAGES_OUTPUT_FOLDER}/{filename}.png')
@@ -141,31 +141,31 @@ if __name__ == "__main__":
     # funct_df.to_csv(f'{CSV_OUTPUT_FOLDER}/output_primes_fucnt_test.csv')
 
 
-    # results_df = pd.read_csv(f'{CSV_OUTPUT_FOLDER}/output_primes.csv')
+    results_df = pd.read_csv(f'{CSV_OUTPUT_FOLDER}/output_primes.csv')
 
-    # avg = results_df.groupby('order')['time'].mean()  # average per order
+    avg = results_df.groupby('order')['time'].mean()  # average per order
 
-    # plot_performance(
-    #     avg.index,
-    #     avg.values,
-    #     filename='scatter_plot_primes',
-    #     title='Relación entre Longitud del Número y Tiempo de Ejecución',
-    #     xlabel='Longitud del Número',
-    #     ylabel='Tiempo de Ejecución (segundos)'
-    # )
+    plot_performance(
+        avg.index,
+        avg.values,
+        filename='scatter_plot_primes',
+        title='Relación entre Longitud del Número y Tiempo de Ejecución',
+        xlabel='Longitud del Número',
+        ylabel='Tiempo de Ejecución (segundos)'
+    )
 
-    # primes = results_df.loc[results_df['result'] == True]  # only true values
-    # plot_performance(
-    #     primes['n'],
-    #     primes['time'],
-    #     filename='scatter_plot_primes_only_primes',
-    #     title='Relación entre números primos y Tiempo de Ejecución',
-    #     xlabel='n',
-    #     ylabel='Tiempo de Ejecución (segundos)'
-    # )
+    primes = results_df.loc[results_df['result'] == True]  # only true values
+    plot_performance(
+        primes['n'],
+        primes['time'],
+        filename='scatter_plot_primes_only_primes',
+        title='Relación entre números primos y Tiempo de Ejecución',
+        xlabel='n',
+        ylabel='Tiempo de Ejecución (segundos)'
+    )
 
 
-    # GCD
+    #GCD
 
     
     results_df = perf_tests(gcdlib.gcd_factorize, 10e8, nargs=2, iterations=50).sort_values(by='a', ignore_index=True)
